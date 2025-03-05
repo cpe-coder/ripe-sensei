a1cef7a30b1af236a602d432f42b2ae2ff127efc
Grian Gajila <griangajila2@gmail.com>
2025-03-05 22:21:44 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex bg-blue-200">
+			<Text>Edit app/index.tsx to edit this screen.</Text>
+		</View>
+	);
+}
