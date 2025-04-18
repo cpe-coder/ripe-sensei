fc9c26d9a3fe91e79be245ff8f2a439b190c2934
Grian Gajila <griangajila2@gmail.com>
2025-04-18 18:21:41 +0800

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
 	return (
 		<View className="flex bg-blue-200">
-			<Text>Edit app/index.tsx to edit this screen.</Text>
+			<Text>Hello World.</Text>
 		</View>
 	);
 }
