d41a41a757a01fb681dd4902d3cfeb149e9dce6d
Grian Gajila <griangajila2@gmail.com>
2025-04-23 21:25:56 +0800

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
+import { AuthProvider } from "@/context/auth-context";
 import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-	return <Stack />;
+	return (
+		<AuthProvider>
+			<Stack>
+				<Stack.Screen options={{ headerShown: false }} name="index" />
+				<Stack.Screen options={{ headerShown: false }} name="(auth)" />
+				<Stack.Screen options={{ headerShown: false }} name="(tabs)" />
+			</Stack>
+		</AuthProvider>
+	);
 }
