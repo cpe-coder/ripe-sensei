da11ea043cf0bd40175c346724bd9de77d98ffde
Grian Gajila <griangajila2@gmail.com>
2025-05-01 23:44:24 +0800

    wip

--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,6 @@ import CustomButton from "./custom-button";
 import DrawerIcon from "./drawer-icon";
 import Header from "./header";
 import InputField from "./input-fields";
-import Loading from "./loading";
 import Logout from "./logout";
 import ProfileInfo from "./profile-info";
 import Settings from "./settings";
@@ -13,7 +12,6 @@ export {
 	DrawerIcon,
 	Header,
 	InputField,
-	Loading,
 	Logout,
 	ProfileInfo,
 	Records,
