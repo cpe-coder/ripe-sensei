f31384a254bda5d39a875199e641a7189b4b335a
Grian Gajila <griangajila2@gmail.com>
2025-05-04 23:24:27 +0800

    wip

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// metro.config.js
+const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+config.transformer = {
+	...config.transformer,
+	babelTransformerPath: require.resolve("nativewind/babel"),
+};
+
+module.exports = withNativeWind(config);
